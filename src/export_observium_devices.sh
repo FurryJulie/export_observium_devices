de5c6e14f6e158ff50ab3ff3dbef23799f4a97bb
Julie Daligaud <furryjulie@fpsource.space>
2019-04-24 12:17:31 +0200

    fix in exec script

--- src/export_observium_devices.sh
+++ src/export_observium_devices.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SQL_SCRIPT = $DIR + "export_observium_devices.sql"
-NOW = `date -u +%s`
-TMP_EXPORT = "/tmp/" + $NOW + "_export_observium_devices.txt"
+#set -eux
 
-/usr/bin/mysql -uroot -p < $SQL_SCRIPT > $TMP_EXPORT
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SQL_SCRIPT="${DIR}export_observium_devices.sql"
+NOW="$(date -u +%s)"
+TMP_EXPORT="/tmp/${NOW}_export_observium_devices.txt"
+
+
+OUTPUT=$(/usr/bin/mysql -uroot -p < $SQL_SCRIPT)
+echo $(sed -i "s/	/,/g" "$OUTPUT") > $EXPORT
