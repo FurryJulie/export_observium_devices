af3b909deebc864bda4142f41c5e515111c261e0
Julie Daligaud <furryjulie@fpsource.space>
2019-04-24 12:19:32 +0200

    fix export path

--- src/export_observium_devices.sh
+++ src/export_observium_devices.sh
@@ -4,8 +4,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SQL_SCRIPT="${DIR}export_observium_devices.sql"
-NOW="$(date -u +%s)"
-TMP_EXPORT="/tmp/${NOW}_export_observium_devices.txt"
+EXPORT="${DIR}/export_observium_devices.csv"
 
 
 OUTPUT=$(/usr/bin/mysql -uroot -p < $SQL_SCRIPT)
