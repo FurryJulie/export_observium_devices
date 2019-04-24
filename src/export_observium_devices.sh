4fdad3840204434e32e35ae817aadffdd0491056
Julie Daligaud <furryjulie@fpsource.space>
2019-04-24 13:06:18 +0200

    fix in exec script

--- src/export_observium_devices.sh
+++ src/export_observium_devices.sh
@@ -3,9 +3,10 @@
 #set -eux
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SQL_SCRIPT="${DIR}export_observium_devices.sql"
+SQL_SCRIPT="${DIR}/export_observium_devices.sql"
 EXPORT="${DIR}/export_observium_devices.csv"
 
 
 OUTPUT=$(/usr/bin/mysql -uroot -p < $SQL_SCRIPT)
-echo $(sed -i "s/	/,/g" "$OUTPUT") > $EXPORT
+
+echo "$( echo  "${OUTPUT}" | sed "s/	/,/g")" > $EXPORT
